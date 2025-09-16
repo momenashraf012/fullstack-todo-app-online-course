db4257f8b4347bb1fecc6d1551b82c549bada724
momen ashraf <momwna13@gmail.com>
2025-09-17 02:20:07 +0300

    Handle User Register With Feedback

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,7 +4,10 @@ import Input from "../components/ui/Input";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { REGISTER_FORM } from "../data";
 import { RegisterSchema } from "../Valitation";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import AxiosInstance from "../confing/axios.confing";
+import toast from "react-hot-toast";
+import { useState } from "react";
 interface IFormInput {
   username: string;
   email: string;
@@ -13,17 +16,43 @@ interface IFormInput {
 
 // Renders
 const RegisterPage = () => {
+  const [isloading, setIsLoading] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IFormInput>(
-    {resolver: yupResolver(RegisterSchema)}
-  );
+  } = useForm<IFormInput>({ resolver: yupResolver(RegisterSchema) });
 
   // Handler
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     console.log(data);
+
+    //panding
+    setIsLoading(true);
+    try {
+      //fulfilled
+      const { status } = await AxiosInstance.post("/auth/local/register", data);
+
+      if (status === 200) {
+        toast.success(
+          "You will navigate to the login page after 2 seconds to login.",
+          {
+            position: "bottom-center",
+            duration: 1500,
+            style: {
+              backgroundColor: "black",
+              color: "white",
+              width: "fit-content",
+            },
+          }
+        );
+      }
+    } catch (error) {
+      //rejected
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   //rendler
@@ -32,7 +61,7 @@ const RegisterPage = () => {
       return (
         <div key={intdex}>
           <Input placeholder={placeholder} {...register(name, validation)} />
-          {errors[name] && <ErrorMassage msg={errors[name].message}/>}
+          {errors[name] && <ErrorMassage msg={errors[name].message} />}
         </div>
       );
     }
@@ -47,12 +76,8 @@ const RegisterPage = () => {
       </h2>
 
       <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-        
-
-     
-
         {rendlerRejister}
-        <Button fullWidth>REister</Button>
+        <Button fullWidth> {isloading ? "loading" : "Register"} </Button>
       </form>
     </div>
   );
