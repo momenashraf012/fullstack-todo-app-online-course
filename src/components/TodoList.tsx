9b403921c36f8171363403aa64b877e23946b24a
momen ashraf <momwna13@gmail.com>
2025-09-18 22:19:23 +0300

    Fetch Data Using useEffect

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import Button from "./ui/Button";
+import AxiosInstance from "../confing/axios.confing";
 
 const TodoList = () => {
+  const [todoData, setTodoData] = useState([]);
+  const storageKey = "LoggedInUser";
+  const userDataString = localStorage.getItem(storageKey);
+  const userData = userDataString ? JSON.parse(userDataString) : null;
+
+  useEffect(() => {
+    try {
+      AxiosInstance.get("/users/me?populate=todos", {
+        headers: {
+          Authorization: `Bearer ${userData.jwt}`,
+        },
+      })
+        .then((rea) => setTodoData(rea.data.todos))
+        .catch((error) => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
+  }, [userData.jwt]);
+
   return (
-    <div className="flex  justify-between items-center">
-      <h1 className="text-lg">title</h1>
-      <div className="flex gap-2">
-        <Button> Edit </Button>
-        <Button className="bg-red-800 "> Cancel </Button>
-      </div>
+    <div className="space-y-3">
+      {todoData.map((input) => (
+        <div key={input.id} className="flex  justify-between items-center">
+          <h1 className="text-lg">{input.title}</h1>
+          <div className="flex gap-2">
+            <Button> Edit </Button>
+            <Button className="bg-red-800 "> Cancel </Button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
