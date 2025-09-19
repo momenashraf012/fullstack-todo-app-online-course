ff5671387f31d2505a502f2338976978df8216f5
momen ashraf <momwna13@gmail.com>
2025-09-19 04:50:56 +0300

    useQuery Hook

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,47 +1,37 @@
-import { useEffect, useState } from "react";
 import Button from "./ui/Button";
 import AxiosInstance from "../confing/axios.confing";
+import { useQuery } from "@tanstack/react-query";
 
 const TodoList = () => {
-  const [todoData, setTodoData] = useState([]);
-  const [lodding, setLodding] = useState(true);
   const storageKey = "LoggedInUser";
   const userDataString = localStorage.getItem(storageKey);
   const userData = userDataString ? JSON.parse(userDataString) : null;
 
-  useEffect(() => {
-    try {
-      AxiosInstance.get("/users/me?populate=todos", {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await AxiosInstance.get("/users/me?populate=todos", {
         headers: {
           Authorization: `Bearer ${userData.jwt}`,
         },
-      })
-        .then((rea) => setTodoData(rea.data.todos))
-        .catch((error) => console.log(error));
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLodding(false);
-    }
-  }, [userData.jwt]);
-
-  if (lodding) return <h1> Lodding..... </h1>;
+      });
+      return data;
+    },
+  });
+  console.log(data);
+  if (isPending) return "Loading...";
 
   return (
     <div className="space-y-3">
-      {todoData.length ? (
-        todoData.map((todo) => (
-          <div key={todo.id} className="flex  justify-between items-center">
-            <h1 className="text-lg">{todo.title}</h1>
-            <div className="flex gap-2">
-              <Button> Edit </Button>
-              <Button className="bg-red-800 "> Cancel </Button>
-            </div>
+      {data.todos.map((todo) => (
+        <div key={todo.id} className="flex  justify-between items-center">
+          <h1 className="text-lg">{todo.title}</h1>
+          <div className="flex gap-2">
+            <Button> Edit </Button>
+            <Button className="bg-red-800 "> Cancel </Button>
           </div>
-        ))
-      ) : (
-        <h1> No todo yet... </h1>
-      )}
+        </div>
+      ))}
     </div>
   );
 };
